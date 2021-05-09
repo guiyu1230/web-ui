c9cc994f61d86f40cf37988278faaba9e9afed9c
guiyu1230 <381564519@qq.com>
2021-05-09 17:41:33 +0800

    组件库文档优化

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,43 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async (config) => {
+    const rules = config.module.rules;
+    rules.push({
+      test: /\.(ts|tsx)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [require.resolve('babel-preset-react-app')],
+          }
+        }
+      ]
+    });
+    rules.push({
+      test: /\.less$/,
+      exclude: /node_modules/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'less-loader'
+      ]
+    })
+    return config;
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  }
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,17 @@
+import { configure } from '@storybook/react';
+import "../src/index.less";
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+// export const parameters = {
+//   actions: { argTypesRegex: "^on[A-Z].*" },
+// }
+
+// 将welcome文档说明置于顶部
+const loadFn = () => {
+  const allExports = [require('../src/Welcome.stories.tsx')];
+  const req = require.context('../src/stories', true, /\.stories\.tsx$/);
+  req.keys().forEach((fname) => allExports.push(req(fname)));
+  return allExports;
 }
+
+// automatically import all files ending in *.stories.tsx
+configure(loadFn, module);
